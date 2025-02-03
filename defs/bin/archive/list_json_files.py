b10f0758402f51cc6cc9d403ad5f2f6481996814
tholonia <tholonia@mailfence.com>
2025-02-03 16:40:53 -0300

    25-02-03 16:40 cleanup

--- defs/bin/archive/list_json_files.py
+++
@@ -1,8 +0,0 @@
-import os
-import glob
-
-directory = "/home/jw/src/iching_cli/defs/alls/out/update1"
-json_files = glob.glob(os.path.join(directory, "*.json"))
-
-for file in sorted(json_files):
-    print(file)
